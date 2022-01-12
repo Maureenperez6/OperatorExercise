202bac93827498d22a1894d13ff7c8955cfc5aa7
Maureen Perez <maureen8694@gmail.com>
2022-01-11 21:55:33 -0500

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+     class Program
+    {
+        static void Main(string[] args)
+        { 
+          
+            int a = 17;
+          
+            int b = 4;
+           
+           
+            int quotient = a / b;
+           
+
+            int remainder = a % b;
+
+            if (a == 17 && b == 4) 
+          
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            
+            double area = AreaOfCircle(radius);
+
+            Console.WriteLine($" The area of a circle with radius of {radius} is {area}");
+
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            double pi = Math.PI;
+            return pi * radius * radius;
+        }
+    }
+}
